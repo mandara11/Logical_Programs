1d9e746282c58887c12d1b99237ebe1ea56ebf92
mandara11 <mandaram2345@gmail.com>
2022-08-02 14:05:53 +0530

    CheckPrimeNumber

---
+++ PrimeNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class PrimeNumber
+    {
+        public static void CheckPrimeNumber()
+        {
+            int count = 0;
+
+            Console.WriteLine("Check the Number is Prime or not");
+
+            Console.Write("Entere the Number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine(num + " is Prime Number\n");
+            }
+            else
+            {
+                Console.WriteLine(num + " is Not Prime Number\n");
+            }
+        }
+    }
+}
