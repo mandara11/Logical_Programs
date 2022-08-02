05fcdefa11fcc8488af341a6329f7dcb7893d717
mandara11 <mandaram2345@gmail.com>
2022-08-02 12:49:49 +0530

    CalculateFibonacciSeries

---
+++ FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public static class FibonacciSeries
+    {
+        public static void ClaculateFibonacciSeries()
+        {
+            int number, n1 = 0, n2 = 1, n3, i;
+
+            Console.WriteLine("Enetr the number of element");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+
+            for(i=2; i<number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3+" ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
