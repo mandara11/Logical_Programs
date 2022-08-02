215f7f8ea5c18b7e5aea4dff0bc1a3a1861e4744
mandara11 <mandaram2345@gmail.com>
2022-08-02 14:10:39 +0530

    Reverse a Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public static class ReverseNumber
+    {
+        public static void Reverse()
+        {
+            int n, reverse = 0, rem;
+
+            Console.WriteLine("Enter the number :");
+            n = int.Parse(Console.ReadLine());
+
+            while(n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reversed Number is : " + reverse);
+        }
+    }
+}
